6e04768d8809ba64d461cb525797b4118c4e3e29
Lasnor <39701348+leandrolasnor@users.noreply.github.com>
2024-04-03 04:52:49 +0000

    refactor: deep_copy method

--- listeners/event_listener.rb
+++ listeners/event_listener.rb
@@ -4,15 +4,19 @@ class EventListener
   def on_supply(event)
     payload = event[:payload]
     res = ::SupplyAction.new.(payload:)
-    puts res.exception.message if res.failure?
-    $state << res.value! if res.success?
+    $state << deep_copy(res.value!) if res.success?
   end
 
   def on_withdraw(event)
     payload = event[:payload]
     res = ::WithdrawAction.new.(payload:)
-    puts res.exception.message if res.failure?
     $last_withdraw = payload if res.success?
-    $state << res.value! if res.success?
+    $state << deep_copy(res.value!) if res.success?
+  end
+
+  private
+
+  def deep_copy(o)
+    Marshal.load(Marshal.dump(o))
   end
 end
